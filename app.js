7450c7cc4b6728e8c2249bd807f481b568670c47
caioportela <caio_fabiio@outlook.com>
2020-07-11 19:55:12 -0300

    Setup application

---
+++ app.js
@@ -0,0 +1,27 @@
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Initialize logger
+const logger = require('./src/loaders/logger');
+
+// Initialize responses
+const responses = require('./src/loaders/responses');
+
+// Initialize models
+require('./src/loaders/models');
+
+const app = express();
+
+app.use(responses);
+app.use(bodyParser.json());
+
+// Initialize routes
+require('./src/routes')(app);
+
+app.use(morgan('dev'));
+app.set('json spaces', 2);
+
+const server = app.listen(3000, () => {
+  logger.info('App listening at port %s', server.address().port);
+});
