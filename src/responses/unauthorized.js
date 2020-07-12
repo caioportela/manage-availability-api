b0fffc130b6f6be762b8c230f96175f574e04bc9
caioportela <caio_fabiio@outlook.com>
2020-07-12 02:56:14 -0300

    Add unauthorized response

---
+++ src/responses/unauthorized.js
@@ -0,0 +1,12 @@
+/**
+ * 401 (Unauthorized) Response
+ *
+ * @param  {String} data
+**/
+
+const logger = require('../loaders/logger');
+
+module.exports = function sendUnauthorized(data) {
+  logger.debug(`Sending 401 ("Unauthorized") response`);
+  return this.res.status(401).send(data);
+};
